819ff413ee4bdd5c76c36ad28ee99b6d4992c223
chairuddinsyah <chairuddinsyah@10.100.8.50>
2016-08-30 17:24:18 +0700

    Update Commit

--- application/controllers/Signin.php
+++ application/controllers/Signin.php
@@ -11,11 +11,17 @@ class Signin extends CI_Controller {
 
     public function index() {
         $data['message'] = "";
+        $base_alias = $this->get_base_alias();
         if (isset($_POST['username']) && isset($_POST['password'])) {
             $data = $this->admin->signin($_POST['username'], $_POST['password']);
             if (is_object($data) || is_array($data)) {
-                $_SESSION['signin'] = $data;    
-                redirect('');
+                $roles = json_decode(isset($data['roles']) ? $data['roles'] : '[]');
+                if (in_array($base_alias, $roles) || $data['username'] == 'admin') {
+                    $_SESSION['signin'] = $data;
+                    redirect('');
+                } else {
+                    $data['message'] = "Unauthorized.";
+                }
             } else {
                 $data['message'] = "Sign In Failed.";
             }
@@ -27,8 +33,14 @@ class Signin extends CI_Controller {
         session_destroy();
         redirect('signin');
     }
-    
+
+    public function get_base_alias() {
+        $arr_base_url = explode("/", base_url());
+        $base_alias = $arr_base_url[count($arr_base_url) - 2];
+        return $base_alias;        
+    }
     public function info() {
         phpinfo();
     }
+
 }
